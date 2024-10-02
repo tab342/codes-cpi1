2a82faf3f676149a36ff9a321a1c5828b2a3f79b
tab342 <muhkjhtvjuytg@gmail.com>
2024-10-02 17:35:10 +0400

    update setup.ps1

--- clone_update/setup.ps1
+++ clone_update/setup.ps1
@@ -1,7 +1,20 @@
 winget install Git.Git
+$OGpath = Get-Location
 $path = Read-Host "Collez le chemin d'acces"
-cd "$path"
+try {
+    cd "$path"
+} catch {
+    Write-Host "Dossier introuvable. Merci de recommencer avec un chemin d'accès valide."
+    PAUSE
+    exit
+}
 git clone https://github.com/tab342/codes-cpi1.git
+cd $OGpath
+if (Test-Path "../clone_update") {
+    Remove-Item "../clone_update" -Recurse -Force
+} else {
+    Remove-Item "./setup.bat", "./setup.ps1", "update.bat", "update.ps1" -Force -ErrorAction SilentlyContinue
+}
 clear
 Write-Host "Installation terminee!"
 PAUSE
