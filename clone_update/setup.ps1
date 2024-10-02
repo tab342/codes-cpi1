297ba5ffb0fad98aba8c298fd88ee2ff6beccd50
tab342 <muhkjhtvjuytg@gmail.com>
2024-10-02 18:34:06 +0400

    Update Setup

--- clone_update/setup.ps1
+++ clone_update/setup.ps1
@@ -10,10 +10,10 @@ try {
 }
 git clone https://github.com/tab342/codes-cpi1.git
 cd $OGpath
-if (Test-Path "../clone_update") {
-    Remove-Item "../clone_update" -Recurse -Force
+if (Test-Path "../setup") {
+    Remove-Item "../setup" -Recurse -Force
 } else {
-    Remove-Item "./setup.bat", "./setup.ps1", "update.bat", "update.ps1" -Force -ErrorAction SilentlyContinue
+    Remove-Item "./setup.bat", "./setup.ps1" -Force -ErrorAction SilentlyContinue
 }
 clear
 Write-Host "Installation terminee!"
